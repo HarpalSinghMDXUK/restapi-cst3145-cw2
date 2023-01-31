17a58b580323e723848573e7c6d08f9e7115af33
Harpal Singh <hsmdxworks@gmail.com>
2023-01-31 05:23:21 +0000

    Update to run at elasticbeanstalk

--- app.js
+++ app.js
@@ -157,6 +157,7 @@ app.use(function(req, res){
 });
 
 // http.createServer(app).listen(3000);
-app.listen(3000, "0.0.0.0", function(){
-    console.log("App is started on 3000")
+const port = process.env.PORT || 3000;
+app.listen(port, function(){
+    console.log("App is started on :"+port);
 });
